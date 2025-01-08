474919a3955148ceed7b50ec58bfeacbc0284cdc
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-09 01:07:53 +0900

    No.0704 Add other solution

--- [0704] Binary Search/Solution.py
+++ [0704] Binary Search/Solution.py
@@ -24,3 +24,32 @@ class Solution:
                 return a  + reculsive
 
         
+# 내 반복문 풀이, O(log n)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        a = len(nums)//2
+        high = len(nums)-1
+        low = 0
+        while low<=high:
+            if target == nums[a]:
+                return a
+
+            if high == low and target != nums[a]:
+                return -1 
+
+            if target > nums[a]:
+                low = a
+                if (high+low)//2 == low:
+                    low = high
+                    a = high
+                else:
+                    a = (high+low)//2
+
+            elif target < nums[a]:
+                high = a
+                if (high+low)//2 == high:
+                    high = low
+                    a = high
+                else:
+                    a = (high+low)//2
+        return -1
